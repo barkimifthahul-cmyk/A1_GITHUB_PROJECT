f6c632ce21bf6a35bec92568785a106fbf6533a8
Mifthah <barkimifthahul@gmail.com>
2026-09-24 13:57:42 +0700

    Update MainActivity

--- app/src/main/java/com/a1stock/app/MainActivity.kt
+++ app/src/main/java/com/a1stock/app/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.a1stock.app
 
 import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
